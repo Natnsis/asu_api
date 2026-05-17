2763005e4e9f755c03a83e0cab3a5e3020f2f65b
Natnael <nsisay49@gmail.com>
2026-05-17 21:51:49 +0300

    chore: dbs

--- config/database.go
+++ config/database.go
@@ -1,11 +1,30 @@
 package config
 
 import (
+	"fmt"
 	"log"
-	"gorm.io/driver/sqlite"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var ConnectDb () {
-	err := go
+var DB *gorm.DB
+
+func ConnectDb() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error, cant get dontenv file")
+	}
+
+	db_url := os.Getenv("DATABASE_URL")
+	conn, err := gorm.Open(postgres.Open(db_url), &gorm.Config{})
+	if err != nil {
+		log.Fatal("couldnt connect to db")
+	}
+
+	fmt.Println("connected to db")
+
+	DB = conn
 }
