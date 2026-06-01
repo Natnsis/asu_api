91beaafcc545155cb22f5aaa761d5ee45d9fe29c
Natnael <nsisay49@gmail.com>
2026-06-01 12:25:42 +0300

    lounge and profile handlers

--- internals/handlers/profile.go
+++ internals/handlers/profile.go
@@ -1,1 +1,71 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"UniCore/internals/db"
+	"UniCore/internals/models"
+
+	"github.com/gorilla/mux"
+)
+
+func CreateProfile(w http.ResponseWriter, r *http.Request) {
+	var profile models.Profile
+	err := json.NewDecoder(r.Body).Decode(&profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	result := db.DbConnection.Create(&profile)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(profile)
+}
+
+func GetSingleProfile(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var profile models.Profile
+	result := db.DbConnection.First(&profile, id)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(profile)
+}
+
+func UpdateProfile(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var profile models.Profile
+	result := db.DbConnection.First(&profile, id)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusBadRequest)
+		return
+	}
+	profile.ID = uint(id)
+	result = db.DbConnection.Save(&profile)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(profile)
+}
