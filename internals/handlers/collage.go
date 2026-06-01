bcfe8a07264815c316f12909bef23a7b5185b3fe
Natnael <nsisay49@gmail.com>
2026-06-01 12:00:20 +0300

    course and colalge handlers

--- internals/handlers/collage.go
+++ internals/handlers/collage.go
@@ -1,5 +1,104 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
 
-func CreateCollage(w http.ResponseWriter, r *http.Request) {}
+	"UniCore/internals/db"
+	"UniCore/internals/models"
+
+	"github.com/gorilla/mux"
+)
+
+func CreateCollage(w http.ResponseWriter, r *http.Request) {
+	var collage models.College
+	err := json.NewDecoder(r.Body).Decode(&collage)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	result := db.DbConnection.Create(&collage)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(collage)
+}
+
+func GetCollage(w http.ResponseWriter, r *http.Request) {
+	var collage models.College
+	result := db.DbConnection.Find(&collage)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(collage)
+}
+
+func GetSingleCollage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var collage models.College
+	result := db.DbConnection.First(&collage, id)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(collage)
+}
+
+func UpdateCollage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var collage models.College
+	result := db.DbConnection.First(&collage, id)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusBadRequest)
+		return
+	}
+	collage.ID = uint(id)
+	result = db.DbConnection.Save(&collage)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(collage)
+}
+
+func DeleteCollage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var collage models.College
+	result := db.DbConnection.Delete(&collage, id)
+	if result != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	if result.RowsAffected == 0 {
+		http.Error(w, "Collage not found", http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
